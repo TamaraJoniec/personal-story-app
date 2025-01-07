984c1af4f162b434e5693e9d529bf763cc68fcf3
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2025-01-07 15:11:09 +0200

    Make video modal square

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,29 +7,28 @@ const Modal = ({ image, video, text, isOpen, onClose }) => {
         isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
       }`}
     >
-      <div className={`border border-black bg-white w-3/4 static flex justify-center p-10 rounded-lg`}>
+      <div className={`border border-black bg-white static flex justify-center p-10 rounded-lg`}>
         <div
-          className={`border border-red-600 bg-white rounded-lg overflow-hidden shadow-lg max-w-sm w-full transition-transform transform duration-300 ease-in-out ${
+          className={`border border-red-600 bg-white rounded-lg overflow-hidden shadow-lg max-w-sm transition-transform transform duration-300 ease-in-out ${
             isOpen ? 'scale-100' : 'scale-95'
           }`}
         >
           {/* Close Button */}
-          <button onClick={onClose} className='relative cursor-pointer text-red-500 z-1'>
+          <button onClick={onClose} className='relative cursor-pointer text-gray-100-500 z-1'>
             CLOSE
           </button>
-
           {/* Image or Video Content */}
-          <div className='w-full h-full'>
-            {image && <img src={image} alt='Post' className='object-cover w-full' />}
+          <div className='w-full h-full flex justify-center items-center'>
+            {image && <img src={image} alt='Post' className='object-contain w-full h-full' />}
             {video && (
-              <video controls className='object-cover w-full'>
+              <video controls className='object-contain w-64 h-64'>
                 <source src={video} type='video/mp4' />
                 Your browser does not support the video tag.
               </video>
             )}
-            {/* Text Description */}
-            {text && (
-              <div className='p-4 text-center text-gray-700'>
+            {!image && !video && text && (
+              <div className='flex justify-center items-center object-contain p-4 text-center text-gray-700'>
+                {' '}
                 <p>{text}</p>
               </div>
             )}
